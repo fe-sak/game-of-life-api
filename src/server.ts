caf439408cf2879b810c5edfccab955cf0e4cee1
Felipe Sakuma <fycorsak@gmail.com>
2022-05-19 19:29:29 -0300

    chore: fix port variable

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app.js';
 
 const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(port, () => {
   console.log(`Listening on ${port}...`);
 });
